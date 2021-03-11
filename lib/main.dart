f4c7e8176734d2dddeef1f5bbe65e17814cd8e4b
Anurag Sharma <anurag.asy81@gmail.com>
2021-03-11 22:25:20 +0530

    Hello World Center

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// The Main function is starting point for all our Flutter App.
+void main() {
+  runApp(MaterialApp(
+    home: Center(
+      child: Text("Hello World"),
+    ),
+  ));
+}
